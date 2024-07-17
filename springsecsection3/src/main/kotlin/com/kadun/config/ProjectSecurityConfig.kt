b2cb73488959afb16ea258a07834df9bc6d9dbe8
kadun1 <kadunjj@gmail.com>
2024-07-17 20:14:45 +0900

    Spring Security : InMemory User Create

--- springsecsection3/src/main/kotlin/com/kadun/config/ProjectSecurityConfig.kt
+++ springsecsection3/src/main/kotlin/com/kadun/config/ProjectSecurityConfig.kt
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -20,5 +23,27 @@ class ProjectSecurityConfig {
             .formLogin(Customizer.withDefaults())
             .httpBasic(Customizer.withDefaults())
         return http.build()
+    }
+
+    @Bean
+    fun userDetailService(): InMemoryUserDetailsManager {
+
+        /**
+         * Approach I where we use withDefaultPasswordEncoder() method
+         * while creating the user details
+         */
+
+        val admin: UserDetails = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("12345")
+            .authorities("admin")
+            .build()
+        val user: UserDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("12345")
+            .authorities("read")
+            .build()
+
+        return InMemoryUserDetailsManager(admin, user)
     }
 }
