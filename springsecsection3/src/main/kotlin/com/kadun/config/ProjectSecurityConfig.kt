3066f73eda1d9f751adf219f134cec205efd2294
kadun1 <kadunjj@gmail.com>
2024-07-30 21:45:17 +0900

    Spring Security : comment inmemory

--- springsecsection3/src/main/kotlin/com/kadun/config/ProjectSecurityConfig.kt
+++ springsecsection3/src/main/kotlin/com/kadun/config/ProjectSecurityConfig.kt
@@ -6,10 +6,13 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
 
 @Configuration
 class ProjectSecurityConfig {
@@ -26,14 +29,14 @@ class ProjectSecurityConfig {
         return http.build()
     }
 
-    @Bean
+    /*@Bean
     fun userDetailService(): InMemoryUserDetailsManager {
 
-        /**
+        *//**
          * Approach 1 where we use withDefaultPasswordEncoder() method
          * while creating the user details
-         */
-        /*
+         *//*
+        *//*
         val admin: UserDetails = User.withDefaultPasswordEncoder()
             .username("admin")
             .password("12345")
@@ -46,11 +49,11 @@ class ProjectSecurityConfig {
             .build()
 
         return InMemoryUserDetailsManager(admin, user)
-        */
+        *//*
 
-        /**
+        *//**
          * Approach 2 where we use NoOpPasswordEncoder Bean
-         */
+         *//*
         val admin: UserDetails = User.withUsername("admin")
             .password("12345")
             .authorities("admin")
@@ -60,8 +63,19 @@ class ProjectSecurityConfig {
             .authorities("read")
             .build()
         return InMemoryUserDetailsManager(admin, user)
+    }*/
+
+    @Bean
+    fun userDetailsService(datasource: DataSource) : UserDetailsService {
+        return JdbcUserDetailsManager(datasource)
     }
 
+    /**
+     * NoOpPasswordEncoder is not recommended for production usage
+     * Use only for non-prod
+     *
+     * @return PasswordEncoder
+     */
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return NoOpPasswordEncoder.getInstance()
