ca8dbf396f0b0ce9dc9d119a93a3db8d7e7f529b
kadun1 <kadunjj@gmail.com>
2024-09-11 23:18:35 +0900

    Spring Security : debug setting

--- springsecsection8/src/main/kotlin/com/kadun/EasyBankBackendApplication.kt
+++ springsecsection8/src/main/kotlin/com/kadun/EasyBankBackendApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
-//@EnableWebSecurity // Spring Boot 에서는 어노테이션이 없어도 기본적인 보안 설정이 적용된다.
+@EnableWebSecurity(debug = true) // Spring Boot 에서는 어노테이션이 없어도 기본적인 보안 설정이 적용된다.
 class EasyBankBackendApplication
 
 fun main(args: Array<String>) {
